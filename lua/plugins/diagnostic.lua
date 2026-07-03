41029a25090a4a0b361f62176f5ff8d89fe532cf
Sean Pietz <spietz701@gmail.com>
2026-07-03 14:28:45 -0400

    lazy load diagnostics plugin

--- lua/plugins/diagnostic.lua
+++ lua/plugins/diagnostic.lua
@@ -1,6 +1,6 @@
 return {
   'tiny-inline-diagnostic.nvim',
-  lazy = false,
+  event = 'DeferredUIEnter',
   after = function()
     require('tiny-inline-diagnostic').setup {
       preset = 'modern',
